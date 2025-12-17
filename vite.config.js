3607d6c963a2e50b2d21fdcb68e9fd33006ce903
cloudmc2 <jander2m@gmail.com>
2025-12-17 21:52:40 +0800

    copnfiasdasd

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
-  // DO NOT set `root` — let Vite treat the project root as root
   build: {
     outDir: path.resolve(__dirname, 'dist'), // final build output
     emptyOutDir: true,
     rollupOptions: {
-      // point to your index.html inside src/pages
-      input: path.resolve(__dirname, 'src/pages/index.html'),
+      // this is where the input goes
+      input: {
+        main: path.resolve(__dirname, 'src/pages/index.html'),
+      },
     },
   },
 
